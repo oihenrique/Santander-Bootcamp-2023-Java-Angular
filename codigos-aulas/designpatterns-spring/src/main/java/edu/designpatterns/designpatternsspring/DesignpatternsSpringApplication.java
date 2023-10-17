1c35e183156e3e22326affc15cd168f3ab99e6fa
Henrique <oi.henriquegomes@gmail.com>
2023-10-16 22:12:09 -0300

    add: código das aulas

---
+++ codigos-aulas/designpatterns-spring/src/main/java/edu/designpatterns/designpatternsspring/DesignpatternsSpringApplication.java
@@ -0,0 +1,13 @@
+package edu.designpatterns.designpatternsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignpatternsSpringApplication.class, args);
+	}
+
+}
---
+++ codigos-aulas/designpatterns-spring/src/test/java/edu/designpatterns/designpatternsspring/DesignpatternsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.designpatterns.designpatternsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
